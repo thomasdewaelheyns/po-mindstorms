7094fad31dc9c6cd1b4034901130eb76f49c6ad8
MHGameWork-Laptop <mhgamework@gmail.com>
2011-10-06 18:11:12 +0200

    Moved to better namespace
    Signed-off-by: MHGameWork-Laptop <mhgamework@gmail.com>

--- Robot/src/PenoPlatinum/ActionFrameworkServer.java
+++ Robot/src/PenoPlatinum/ActionFrameworkServer.java
@@ -1,3 +1,5 @@
+package PenoPlatinum;
+
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
---
+++ Robot/src/PenoPlatinum/CalibrateStraightLine.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PenoPlatinum;
+
+/**
+ *
+ * @author MHGameWork
+ */
+public class CalibrateStraightLine implements IAction {
+
+    ActionParameters parameters = new ActionParameters();
+
+    public ActionParameters GetParameters() {
+        return parameters;
+    }
+
+    public void Execute() {
+        
+    }
+}
