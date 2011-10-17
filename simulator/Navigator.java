9f316f451d2f1d82608fe3a1d69c79378b9d6e9a
Christophe VG <xtof@pumpkin.local>
2011-10-17 13:58:18 +0200

    implemented Navigator for VeelhoekRobot

---
+++ simulator/Navigator.java
@@ -0,0 +1,20 @@
+/**
+ * Navigator Interface
+ * 
+ * A Navigator implements how the robot tries to reach its goal. As with a 
+ * true Navigator, the robot will constantly ask it what to do, until the
+ * navigator indicates it has reached its goal.
+ * 
+ * Author: Team Platinum
+ */
+
+public interface Navigator {
+  public static final int NONE = 0;
+  public static final int MOVE = 1;
+  public static final int TURN = 2;
+
+  public Boolean reachedGoal();
+  public int     nextAction();
+  public double  getDistance();
+  public double  getAngle();
+}
---
+++ simulator/VeelhoekNavigatorRobot.java
@@ -0,0 +1,65 @@
+/**
+ * VeelhoekNavigatorRobot
+ * 
+ * This robot implements a basic polygon-driving algorithm, using only motors
+ * and no sensors. It uses an event loop and a Navigator.
+ * 
+ * Author: Team Platinum
+ */
+
+class VeelhoekNavigatorRobot implements Robot {
+
+  private RobotAPI  api;
+  private Navigator navigator;
+
+  private int   edgeCount    = 1; // just turn around
+  private float vertexLength = 0; // stay in place
+
+  public void useRobotAPI( RobotAPI api ) {
+    this.api = api;
+  }
+  
+  public void processCommand( String cmd ) {
+    // this robot doesn't support the reception of commands while it's running
+    // TODO: add errorchecking, we now only deal with correct input
+    String[] parts    = cmd.split( ";" );
+    this.edgeCount    = Integer.parseInt( parts[0] );
+    this.vertexLength = Float.parseFloat( parts[1] );
+  }
+  
+  public String getModelState() {
+    // this robot doesn't use a Model, so no state can be returned.
+    // it also doesn't support retrieval of the status while it's running
+    return "";
+  }
+  
+  public String getNavigatorState() {
+    // TODO
+    return "";
+  }
+
+  /**
+   * This implements the Event Loop. The Robot continously asks the navigator
+   * what to do.
+   */
+  public void run() {
+    this.navigator = new VeelhoekNavigator(this.edgeCount, this.vertexLength);
+    while( ! this.navigator.reachedGoal() ) {
+      switch( this.navigator.nextAction() ) {
+        case Navigator.MOVE:
+          this.api.move(this.navigator.getDistance());
+          break;
+        case Navigator.TURN:
+          this.api.turn(this.navigator.getAngle());
+          break;
+        default:
+          // do nothing
+      }
+    }
+  }
+  
+  public void stop() {
+    // this robot doesn't support stopping, because its run method is executed
+    // synchronously
+  }
+}
