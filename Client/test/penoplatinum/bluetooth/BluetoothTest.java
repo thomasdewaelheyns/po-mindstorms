67a52744affd60e8a895e85685757ab1b8b26fd5
MHGameWork-Laptop <mhgamework@gmail.com>
2011-10-20 11:44:43 +0200

    Implemented basic SimpleConnection tests

---
+++ Client/test/penoplatinum/bluetooth/BluetoothTest.java
@@ -0,0 +1,77 @@
+package penoplatinum.bluetooth;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MHGameWork
+ */
+public class BluetoothTest {
+
+    SimpleConnection ca;
+    SimpleConnection cb;
+
+    public BluetoothTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        ca = new SimpleConnection();
+        cb = new SimpleConnection();
+        ca.setEndPoint(cb);
+        cb.setEndPoint(ca);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testRegisterTransporter() {
+        int packetA = 23;
+
+        ca.RegisterTransporter(new PacketTransporter(ca), packetA);
+
+    }
+
+    @Test
+    public void testSendReceive() throws IOException {
+        int packetA = 23;
+        String msg = "Hellow Universe!";
+
+        PacketTransporter tA = new PacketTransporter(ca);
+        PacketTransporter tB = new PacketTransporter(cb);
+
+        ca.RegisterTransporter(tA, packetA);
+        cb.RegisterTransporter(tB, packetA);
+
+
+        tA.getSendStream().writeUTF(msg);
+        tA.SendPacket(packetA);
+
+        assertEquals(packetA, tB.ReceivePacket());
+        assertEquals(msg, tB.getReceiveStream().readUTF());
+        assertEquals(0, tB.getReceiveStream().available());
+
+
+
+    }
+}
