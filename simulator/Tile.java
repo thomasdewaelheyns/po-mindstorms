90bbcf6aa72d7e48efd6407c21d4f8925b1581e0
Christophe VG <xtof@pumpkin.local>
2011-10-29 14:25:45 +0200

    added Tile class basic internal representation with unit tests

---
+++ simulator/Tile.java
@@ -0,0 +1,163 @@
+/**
+ * Tile
+ * 
+ * Class representing a tile. The internal data is stored as an integer, which
+ * is treathed as a 32bit string, used to represent different information 
+ * about the tile:
+ * 
+ * NESW NESW 123 123 .........
+ * 
+ * wall switches
+ *   4 bits : N E S W
+ * line switches
+ *   4 bits : N E S W
+ * barcode
+ *   4 bits : barcode is hex number 1..F
+ * barcode position
+ *   3 bits : none, N, E, S or W
+ * narrowing orientation
+ *   3 bits : none, N, E, S or W
+ * total = 18 bits, 14 spare for future additional information (ramp,...)
+ *
+ *  Author: Team Platinum
+ */
+
+public class Tile {
+  public static final int N = 0;
+  public static final int E = 1;
+  public static final int S = 2;
+  public static final int W = 3;
+  
+  private int data;
+  
+  public Tile() {
+    this.data = 0x0;
+  }
+  
+  public Tile( int data ) {
+    this.data = data;
+  }
+  
+  /* Walls */
+  public Tile setWall(int location) { 
+    this.setBit(location);
+    return this;
+  }
+  
+  public Tile unsetWall(int location)  { 
+    this.unsetBit(location);
+    return this;
+  }
+  
+  public Boolean hasWall( int location ) {
+    return this.hasBit(location);
+  }
+  
+  /* Lines */
+  public Tile setLine(int location) { 
+    this.setBit(location + 4);
+    return this;
+  }
+  
+  public Tile unsetLine(int location)  { 
+    this.unsetBit(location + 4);
+    return this;
+  }
+  
+  public Boolean hasLine(int location) {
+    return this.hasBit(location + 4);
+  }
+  
+  /* Barcode */
+  public Tile setBarcode( int code ) {
+    this.setBits( 8, 4, code );
+    return this;
+  }
+
+  public Tile unsetBarcode() {
+    this.unsetBits( 8, 4 );
+    return this;
+  }
+
+  public int getBarcode() {
+    return this.getBits(8,4);
+  }
+  
+  public Tile setBarcodeLocation( int location ) {
+    this.setBits( 12, 3, location + 1 );
+    return this;
+  }
+
+  public Tile unsetBarcodeLocation() {
+    this.unsetBits( 12, 3 );
+    return this;
+  }
+
+  public int getBarcodeLocation() {
+    return this.getBits(12,3) - 1;
+  }
+  
+  /* Narrowing */
+  public Tile setNarrowingOrientation( int orientation ) { 
+    this.setBits( 15, 3, orientation + 1 );
+    return this;
+  }
+  
+  public Tile unsetNarrowingOrientation()  { 
+    this.unsetBits(15, 3);
+    return this;
+  }
+  
+  public int getNarrowingOrientation() {
+    return this.getBits(15,3) - 1;
+  }
+
+  /* representations */
+  public int toInteger() {
+    return this.data;
+  }
+
+  public String toString() {
+    String bits = "";
+    for( int i=0; i<32; i++ ) {
+      bits += this.hasBit(i) ? "1" : "0";
+    }
+    return bits;
+  }
+
+  /* elementary bitwise operations */
+
+  // sets one bit at position to 1
+  private void setBit(int p) {
+    this.data |= (1<<p);
+  }
+
+  // sets one bit at position to 0
+  private void unsetBit(int p) {
+    this.data &= ~(1<<p);
+  }
+  
+  // checks if at position the bit is set to 1
+  private Boolean hasBit(int p) {
+    return ( this.data & (1<<p) ) != 0;
+  }
+
+  // sets a range of bits, represented by value
+  private void setBits(int start, int length, int value) {
+    this.unsetBits( start, length );
+    value <<= start;
+    this.data |= value;
+  }
+
+  // sets a range of bits to 0
+  private void unsetBits(int start, int length) {
+    int mask = ( ( 1 << length ) - 1 ) << start;
+    this.data &= ~(mask);
+  }
+
+  // returns a range of length bits starting at start
+  private int getBits(int start, int length) {
+    int mask = ( 1 << length ) - 1;
+    return ( ( this.data >> start ) & mask );
+  }
+}
---
+++ simulator/t/TileTest.java
@@ -0,0 +1,179 @@
+import junit.framework.*; 
+
+public class TileTest extends TestCase { 
+  private Tile tile;
+
+  public TileTest(String name) { 
+    super(name);
+  }
+  
+  public void testNorthWall() {
+    Tile tile = this.createEmptyTile();
+    tile.setWall(Tile.N);
+    assertEquals("10000000000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasWall(Tile.N));
+    tile.unsetWall(Tile.N);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testEastWall() {
+    Tile tile = this.createEmptyTile();
+    tile.setWall(Tile.E);
+    assertEquals("01000000000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasWall(Tile.E));
+    tile.unsetWall(Tile.E);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testSouthWall() {
+    Tile tile = this.createEmptyTile();
+    tile.setWall(Tile.S);
+    assertEquals("00100000000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasWall(Tile.S));
+    tile.unsetWall(Tile.S);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testWestWall() {
+    Tile tile = this.createEmptyTile();
+    tile.setWall(Tile.W);
+    assertEquals("00010000000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasWall(Tile.W));
+    tile.unsetWall(Tile.W);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+  
+  public void testNorthLine() {
+    Tile tile = this.createEmptyTile();
+    tile.setLine(Tile.N);
+    assertEquals("00001000000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasLine(Tile.N));
+    tile.unsetLine(Tile.N);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testEastLine() {
+    Tile tile = this.createEmptyTile();
+    tile.setLine(Tile.E);
+    assertEquals("00000100000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasLine(Tile.E));
+    tile.unsetLine(Tile.E);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testSouthLine() {
+    Tile tile = this.createEmptyTile();
+    tile.setLine(Tile.S);
+    assertEquals("00000010000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasLine(Tile.S));
+    tile.unsetLine(Tile.S);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testWestLine() {
+    Tile tile = this.createEmptyTile();
+    tile.setLine(Tile.W);
+    assertEquals("00000001000000000000000000000000", tile.toString() ); 
+    assertTrue(tile.hasLine(Tile.W));
+    tile.unsetLine(Tile.W);
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+  
+  public void testBarcode() {
+    Tile tile = this.createEmptyTile();
+    tile.setBarcode(15);
+    assertEquals("00000000111100000000000000000000", tile.toString() ); 
+    assertEquals(15, tile.getBarcode());
+    tile.unsetBarcode();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+  
+  public void testBarcodeLocationNorth() {
+    Tile tile = this.createEmptyTile();
+    tile.setBarcodeLocation(Tile.N);
+    assertEquals("00000000000010000000000000000000", tile.toString() ); 
+    assertEquals(Tile.N, tile.getBarcodeLocation());
+    tile.unsetBarcodeLocation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testBarcodeLocationEast() {
+    Tile tile = this.createEmptyTile();
+    tile.setBarcodeLocation(Tile.E);
+    assertEquals("00000000000001000000000000000000", tile.toString() ); 
+    assertEquals(Tile.E, tile.getBarcodeLocation());
+    tile.unsetBarcodeLocation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testBarcodeLocationSouth() {
+    Tile tile = this.createEmptyTile();
+    tile.setBarcodeLocation(Tile.S);
+    assertEquals("00000000000011000000000000000000", tile.toString() ); 
+    assertEquals(Tile.S, tile.getBarcodeLocation());
+    tile.unsetBarcodeLocation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testBarcodeLocationWest() {
+    Tile tile = this.createEmptyTile();
+    tile.setBarcodeLocation(Tile.W);
+    assertEquals("00000000000000100000000000000000", tile.toString() ); 
+    assertEquals(Tile.W, tile.getBarcodeLocation());
+    tile.unsetBarcodeLocation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+  
+  public void testNarrowingOrientationNorth() {
+    Tile tile = this.createEmptyTile();
+    tile.setNarrowingOrientation(Tile.N);
+    assertEquals("00000000000000010000000000000000", tile.toString() ); 
+    assertEquals(Tile.N, tile.getNarrowingOrientation());
+    tile.unsetNarrowingOrientation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testNarrowingOrientationEast() {
+    Tile tile = this.createEmptyTile();
+    tile.setNarrowingOrientation(Tile.E);
+    assertEquals("00000000000000001000000000000000", tile.toString() ); 
+    assertEquals(Tile.E, tile.getNarrowingOrientation());
+    tile.unsetNarrowingOrientation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testNarrowingOrientationSouth() {
+    Tile tile = this.createEmptyTile();
+    tile.setNarrowingOrientation(Tile.S);
+    assertEquals("00000000000000011000000000000000", tile.toString() ); 
+    assertEquals(Tile.S, tile.getNarrowingOrientation());
+    tile.unsetNarrowingOrientation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+
+  public void testNarrowingOrientationWest() {
+    Tile tile = this.createEmptyTile();
+    tile.setNarrowingOrientation(Tile.W);
+    assertEquals("00000000000000000100000000000000", tile.toString() ); 
+    assertEquals(Tile.W, tile.getNarrowingOrientation());
+    tile.unsetNarrowingOrientation();
+    assertEquals("00000000000000000000000000000000", tile.toString() ); 
+  }
+  private Tile createEmptyTile() {
+    return new Tile();
+  }
+
+}
+
+
+/*
+    Tile tile = new Tile();
+    tile.setWall(Tile.W);
+    tile.setLine(Tile.W);
+    tile.setBarcode(7);
+    System.out.println( tile.toString() );
+    tile.setBarcode(9);
+    System.out.println( tile.toString() );
+    System.out.println( tile.getBarcode() );
+
+*/
