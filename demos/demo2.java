4072d308273d96501cec628ddbc68d2b4ab5ae65
Christophe VG <xtof@pumpkin.local>
2011-11-07 14:45:53 +0100

    prepared delivarables for demo2

---
+++ demos/demo2.java
@@ -0,0 +1,21 @@
+/**
+ * demo 2
+ *
+ * Robot is able to
+ * 1) follow a line
+ * 2) follow a wall
+ * 3) read and interprete barcodes
+ * It is controled through the Console.
+ * 
+ * Author: Team Platinum
+ */
+package penoplatinum.demo;
+
+import penoplatinum.veelhoek.Veelhoek;
+import penoplatinum.movement.RotationMovement;
+
+public class demo2 {
+  public static void main(String[] args) {
+    // TODO
+  }
+}
