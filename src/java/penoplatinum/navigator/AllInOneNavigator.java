401f303f34ba9811d8addd0025e9c65753d63185
thomasdewaelheyns <thomas.dewaelheyns@gmail.com>
2011-11-28 14:21:04 +0100

    Added Barcode stuff.
    Signed-off-by: thomasdewaelheyns <thomas.dewaelheyns@gmail.com>

---
+++ src/java/penoplatinum/navigator/AllInOneNavigator.java
@@ -0,0 +1,104 @@
+package penoplatinum.navigator;
+
+import penoplatinum.simulator.Model;
+import penoplatinum.simulator.Navigator;
+
+
+/*
+ * LineFollowerNavigator
+ * 
+ * This navigator uses the lightsensor to follow lines accross the track
+ * 
+ * author: Team Platinum
+ */
+public class AllInOneNavigator implements Navigator {
+
+    public static final int NONE = 0;
+    
+    //This is a bitmask
+    public static final int DRIVE_TO_MIDDLE= 0;
+    public static final int TURN = 1;
+    public static final int LEFT = 2;
+    public static final int ONE_EIGHTY = 3;
+    
+    private Model model;
+    private int next = AllInOneNavigator.NONE;
+    private int current = AllInOneNavigator.NONE;
+
+    @Override
+    //no goal just drive
+    public Boolean reachedGoal() {
+        return false;
+    }
+
+    @Override
+    public int nextAction() {
+        if(!this.model.isMoving() && ((next>>AllInOneNavigator.TURN)&1) == 1){
+            System.out.println("Turning: ");
+            current = next;
+            next = AllInOneNavigator.NONE;
+            return Navigator.TURN;
+        }
+
+        int barcode = model.getBarcode();
+        model.setBarcode(-1);
+        if(barcode == -1){
+            if(this.model.isMoving()){
+                return Navigator.NONE;
+            }
+            return Navigator.MOVE;
+        }
+        //System.out.println("Barcode found: "+barcode);
+        next = AllInOneNavigator.NONE;
+        current = AllInOneNavigator.NONE;
+        switch (barcode) {
+            case 3:
+                next |= 1<<AllInOneNavigator.LEFT;
+            case 6:
+                next |= 1<<AllInOneNavigator.TURN;
+                current |= 1<<AllInOneNavigator.DRIVE_TO_MIDDLE;
+                System.out.println("Turn! "+next+", "+current);
+                return Navigator.MOVE;
+                
+            case 9:  //Wrong direction
+            case 12: //Wrong direction
+            case 14: //Wrong direction
+                next |= 1<<AllInOneNavigator.TURN;
+                next |= 1<<AllInOneNavigator.ONE_EIGHTY;
+                return Navigator.MOVE;
+                
+            case 1: //forward
+            case 2: //forward
+            case 4: //forward
+                
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 11:
+            case 13:
+            case 15:
+            default:
+                return Navigator.MOVE;
+        }
+    }
+    @Override
+    public double getDistance() {
+        double distance = (((current>>AllInOneNavigator.DRIVE_TO_MIDDLE)&1)==1? 0.300 : 0.325);
+        return distance;
+    }
+
+    @Override
+    public double getAngle() {
+        int direction = ((current>>AllInOneNavigator.LEFT<<1)&2)-1;
+        int angle = 90+(current>>AllInOneNavigator.ONE_EIGHTY)&1*90;
+        return direction*angle;
+    }
+    
+    // the model supplies the sensor values so we can decide to drive or turn
+    @Override
+    public AllInOneNavigator setModel(Model model) {
+        this.model = model;
+        return this;
+    }
+}
---
+++ src/java/penoplatinum/simulator/SimulationRunner1.java
@@ -0,0 +1,119 @@
+package penoplatinum.simulator;
+
+import org.apache.commons.cli.*;
+
+/**
+ * SimulationRunner
+ * 
+ * Constructs a robot and course, sets up the Simulator and starts the
+ * simulation.
+ * 
+ * Author: Team Platinum
+ */
+ 
+class SimulationRunner1 {
+  
+  private Simulator simulator;
+  private Robot     robot;
+  private Navigator navigator;
+  
+  public SimulationRunner1() {
+    this.simulator = new Simulator();
+    this.createMap();
+  }
+  
+  // TODO: create MapFactory with support to load files
+  //       add option to provide map file
+  private void createMap() {
+    Map map = new Map(4)
+      .add(Tiles.S_E) .add(Tiles.W_E) .add(Tiles.W_E) .add(Tiles.W_S)
+      .add(Tiles.E_N) .add(Tiles.E_W) .add(Tiles.S_W) .add(Tiles.N_S)
+      .add(Tiles.S_E) .add(Tiles.W_E) .add(Tiles.W_N) .add(Tiles.N_S)
+      .add(Tiles.E_N) .add(Tiles.E_W) .add(Tiles.E_W) .add(Tiles.N_W);
+    this.simulator.useMap(map);
+  }
+  
+  public SimulationRunner1 useUI() {
+    this.simulator.displayOn(new SwingSimulationView());
+    return this;
+  }
+
+  public SimulationRunner1 useNavigator(String navigator) {
+    try {
+      Class theClass  = Class.forName(navigator);
+      this.navigator = (Navigator)theClass.newInstance();
+    } catch ( ClassNotFoundException ex ){
+      System.err.println( ex + " Navigator class must be in class-path.");
+    } catch( InstantiationException ex ){
+      System.err.println( ex + " Navigator class must be concrete.");
+    } catch( IllegalAccessException ex ){
+      System.err.println( ex + " Navigator class must have a no-arg constr.");
+    }
+    return this;
+  }
+
+  public SimulationRunner1 putRobotAt(String position) {
+    // parse position string
+    String[] parts = position.split(",");
+    int x         = Integer.parseInt(parts[0]),
+        y         = Integer.parseInt(parts[1]),
+        direction = Integer.parseInt(parts[2]);   
+    // put the robot
+    if( this.navigator != null ) {
+      Robot robot = new NavigatorRobot(this.navigator);
+      this.simulator.putRobotAt(robot, x, y, direction);
+    } else {
+      System.err.println( "Please provide a navigator, then add a robot." );
+    }
+    return this;
+  }
+  
+  public void start() {
+    this.simulator.run();
+  }
+  
+  public static void showHelpFor(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp( "SimulationRunner", options );    
+  }
+
+  public static void main(String[] args) {
+    String defaultNavigator = "penoplatinum.navigator.AllInOneNavigator";
+    String defaultPosition  = "150,125,90";
+    
+    SimulationRunner1 runner = new SimulationRunner1();
+    Boolean setupComplete = false;
+    
+    // process command line arguments
+    Options options = new Options();
+    options.addOption( "h", "help",  false, "show this helpful information." );
+    options.addOption( "q", "quiet", false, "don't show a user interface." );
+
+    options.addOption( "n", "navigator",true, "use navigator <classname>. " +
+                                              "default=" + defaultNavigator );
+    options.addOption( "p", "put",      true, "put robot at x,y,direction. " +
+                                              "default=" + defaultPosition  );
+
+    CommandLineParser parser = new GnuParser();
+    try {
+      CommandLine line = parser.parse( options, args );
+
+      if( line.hasOption("help") ) { 
+        SimulationRunner1.showHelpFor(options);
+      } else {
+        if( ! line.hasOption("q") ) { runner.useUI(); }
+
+        runner.useNavigator( line.getOptionValue( "navigator", 
+                                                  defaultNavigator ) );
+        runner.putRobotAt( line.getOptionValue( "put", defaultPosition ) );
+
+        setupComplete = true;
+      }
+    } catch( ParseException exp ) {
+      System.err.println( "ERROR:" + exp.getMessage() );
+      SimulationRunner1.showHelpFor(options);
+    }
+
+    if( setupComplete ) { runner.start(); }
+  }
+}
