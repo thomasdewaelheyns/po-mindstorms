72811fb7c49884fc82c8c6ac87e3e25cd9313f1a
Christophe VG <xtof@pumpkin.local>
2011-11-28 18:11:25 +0100

    servlet now accepts multiple connections and support disconnections

--- src/java/penoplatinum/ui/server/RestServlet.java
+++ src/java/penoplatinum/ui/server/RestServlet.java
@@ -18,63 +18,90 @@ import javax.sql.DataSource;
 
 @SuppressWarnings("serial")
 public class RestServlet extends HttpServlet {
-  private InitialContext context;
-  private DataSource     ds;
-  private Connection     con;
-  private ResultSet      rs;
-
-  private Integer last = 0;
 
   public void service( HttpServletRequest request,
-                       HttpServletResponse response ) 
-         throws ServletException, IOException 
+    HttpServletResponse response ) 
+    throws ServletException, IOException 
   {
-    PrintWriter out = response.getWriter();
+    new Streamer().service(request, response);
+  }
 
-    // try to connect to the database
-    if( ! this.setupDS() ) {
-      out.println( "error('failed to connect to DS');" );
-      return;
-    }
-    
-    try {
-      out.println( "start(" + this.last + ");" );
-      while(true) {
-        this.getNextBatch();
-        int id = -1; String ts, robot, message;
-        while(this.rs.next()) {
-          id      = rs.getInt(1);
-          ts      = rs.getString(2);
-          robot   = rs.getString(3);
-          message = rs.getString(4);
-          out.println( "update( '" + ts + "','" + message + "');" );
-          out.flush();
-        }
-        this.rs.close();
-        if( id > this.last ) { this.last = id; }
+  // class wrapping all logic to stream a set of data ...
+  private class Streamer {
+    private InitialContext context;
+    private DataSource     ds;
+    private Connection     con;
+    private ResultSet      rs;
+    private Integer        last = 0;
+
+    public void service( HttpServletRequest request,
+                         HttpServletResponse response ) 
+           throws ServletException, IOException 
+    {
+      PrintWriter out = response.getWriter();
+
+      // try to connect to the database
+      if( ! this.connect() ) {
+        out.println( "error('failed to connect to DS');" );
+        return;
       }
-    } catch( Exception e ) {
-      // nothing todo, in fact normal ending when client disconnects
-      out.println( "error(" + e.toString() + ");" );
-    } finally {
-      out.println( "stop(" + this.last + ");" );
-      out.close();
+
+      try {
+        System.out.println( "Starting stream..." );
+        out.println( "start();" );
+
+        // while we don't encounter any errors writing to the client...
+        while(!out.checkError()) {
+          this.getNextBatch();
+          int id = -1; String ts, robot, message;
+          while(this.rs.next()) {
+            id      = rs.getInt(1);
+            ts      = rs.getString(2);
+            robot   = rs.getString(3);
+            message = rs.getString(4);
+            out.println( "update( '" + ts + "','" + message + "');" );
+          }
+          out.print( " " ); // force output, causing exception when closed
+          out.flush();      // flush to the browser for optimal UI experience
+          this.rs.close();  // close this recordset
+          if( id > this.last ) { this.last = id; } // keep track last sent id
+          Thread.sleep(1);  // and breath
+        }
+      } catch( Exception e ) {
+        // nothing todo, it's in fact normal ending when client disconnects
+      } finally {
+        // after not exposing the exception, clean up
+        System.out.println( "Closing Stream..." );
+        out.close();
+        this.disconnect();
+      }
     }
-  }
-  
-  private Boolean setupDS() {
-    try {
-      this.context = new InitialContext();
-      this.ds      = (DataSource)context.lookup("java:db");
-      this.con     = ds.getConnection();
-    } catch(Exception e) {
-      return false;
+
+    // connects to the datasource
+    private Boolean connect() {
+      try {
+        this.context = new InitialContext();
+        this.ds      = (DataSource)context.lookup("java:db");
+        this.con     = ds.getConnection();
+      } catch(Exception e) {
+        System.out.println( "Couldn't connect to DataSource" );
+        return false;
+      }
+      return true;
     }
-    return true;
-  }
-  
-  private void getNextBatch() throws NamingException, SQLException {
-    final String sql = "SELECT * FROM logs WHERE id > " + this.last + ";";
-    this.rs = this.con.createStatement().executeQuery(sql);
+
+    // disconnects from the datasource
+    private void disconnect() {
+      try {
+        this.con.close();
+      } catch( SQLException e ) {
+        System.out.println( "Couldn't disconnect from DataSource" );
+      }
+    }
+
+    private void getNextBatch() throws NamingException, SQLException {
+      final String sql = "SELECT * FROM logs WHERE id > " + this.last + ";";
+      this.rs = this.con.createStatement().executeQuery(sql);
+    }
   }
 }
