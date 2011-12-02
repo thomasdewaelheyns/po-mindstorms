56071595d65ed26adddb0336fc771c6cda3ba26b
Christophe VG <xtof@pumpkin.local>
2011-12-02 15:28:38 +0100

    updated schema to include all relevant fields

--- src/java/penoplatinum/agent/schema.sql
+++ src/java/penoplatinum/agent/schema.sql
@@ -3,8 +3,19 @@ CREATE DATABASE robots;
 USE robots;
 
 CREATE TABLE logs (
-  id      INTEGER       PRIMARY KEY AUTO_INCREMENT,
-  ts      TIMESTAMP     DEFAULT NOW(),
-  robot   VARCHAR(50)   NOT NULL,
-  message VARCHAR(1000) NOT NULL
+  id            INTEGER       PRIMARY KEY AUTO_INCREMENT,
+  ts            TIMESTAMP     DEFAULT NOW(),
+  robot         VARCHAR(50)   NOT NULL,
+  lightValue    INTEGER,
+  lightColor    VARCHAR(10),
+  sonarAngle    INTEGER,
+  sonarDistance INTEGER,
+  pushLeft      BIT,
+  pushRight     BIT,
+  event         VARCHAR(25),
+  source        VARCHAR(128),
+  plan          VARCHAR(25),
+  queue         VARCHAR(128),
+  action        VARCHAR(25),
+  argument      VARCHAR(25)
 );
