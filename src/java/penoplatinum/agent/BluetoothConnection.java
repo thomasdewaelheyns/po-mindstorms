52540015852515d5b5e8b3522fea3c8eb0bbe38b
Christophe VG <xtof@pumpkin.local>
2011-12-05 11:37:07 +0100

    added transporter for remote logging

--- src/java/penoplatinum/agent/BluetoothConnection.java
+++ src/java/penoplatinum/agent/BluetoothConnection.java
@@ -24,12 +24,26 @@ public class BluetoothConnection {
     this.connection.initializeConnection();
     this.endPoint   = new PacketTransporter(this.connection);
     this.connection.RegisterTransporter(this.endPoint, 123);
+    this.connection.RegisterTransporter(this.endPoint, penoplatinum.Utils.PACKETID_LOG);
   }
 
   public Boolean hasNext() {
-    this.endPoint.ReceivePacket();
-    this.nextMsg = new Scanner(this.endPoint.getReceiveStream()).nextLine();
-    return true;
+    int packet;
+    String data;
+    Boolean logging = true;
+    while(logging) {
+      packet = this.endPoint.ReceivePacket();
+      data   = new Scanner(this.endPoint.getReceiveStream()).nextLine();      
+      switch(packet) {
+        case penoplatinum.Utils.PACKETID_LOG:
+          System.out.println( "Log:>" + data );
+          break;
+        case 123:
+          this.nextMsg = data;
+          return true;
+      }
+    }
+    return false;
   }
   
   public String getMessage() {
