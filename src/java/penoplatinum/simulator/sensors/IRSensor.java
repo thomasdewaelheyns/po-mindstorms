b4af3c62a3874f13403e41e0583cacc954e65e39
rupsbant <rupsbant@hotmail.com>
2012-02-20 16:57:45 +0100

    IR sensor verbeterd

--- src/java/penoplatinum/simulator/sensors/IRSensor.java
+++ src/java/penoplatinum/simulator/sensors/IRSensor.java
@@ -26,19 +26,12 @@ public class IRSensor implements Sensor {
     // Checks if there is a wall in the way that prevents vision of the pacman
     if(distanceToWall <= distanceToPacman) return 0;
 
-    // Checks in which zone the detection occurs
-    if((actualAngle <= 15) || (actualAngle> 345)) return 5;
-    if((actualAngle <= 45) && (actualAngle> 15)) return 6;
-    if((actualAngle <= 75) && (actualAngle> 45)) return 7;
-    if((actualAngle <= 105) && (actualAngle> 55)) return 8;
-    if((actualAngle <= 135) && (actualAngle> 105)) return 9;
-    if((actualAngle <= 345) && (actualAngle> 315)) return 4;
-    if((actualAngle <= 315) && (actualAngle> 285)) return 3;
-    if((actualAngle <= 285) && (actualAngle> 255)) return 2;
-    if((actualAngle <= 255) && (actualAngle> 225)) return 1;
-
-    // if not returned till now, it means the detection is in the "blind spot" of the ir sensor
-    return 0;
+    int angle = (int) ((actualAngle+165)%360/30);
+    if(angle>9){
+      return 0;
+    } else {
+      return angle;
+    }
   }
 
   @Override
