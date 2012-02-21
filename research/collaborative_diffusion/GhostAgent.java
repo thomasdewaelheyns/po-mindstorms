4a87b0813493fef6e6b9cef88e3f9d3d01d60341
Christophe VG <xtof@pumpkin.local>
2012-02-21 18:38:38 +0100

    implemented turning movement on Pacman and Ghost Agents

--- research/collaborative_diffusion/GhostAgent.java
+++ research/collaborative_diffusion/GhostAgent.java
@@ -6,6 +6,10 @@ public class GhostAgent implements Agent {
   
   private int previousMove = -1;
   
+  // turns are executed, then moves are executed
+  private int turns = 0; // number of turns, positive Right, negative Left
+  private int moves = 0; // number of moves
+  
   public GhostAgent(int left, int top, int orientation, String name) {
     this.left        = left;
     this.top         = top;
@@ -41,7 +45,7 @@ public class GhostAgent implements Agent {
     
     // if any of the moves brings us onto the 1000 pos, we hold our position
     if( n == -1000 || e == -1000 || s == -1000 || w == -1000 ) {
-      this.previousMove = -1;
+      this.holdPosition();
       return;
     }
     
@@ -64,20 +68,104 @@ public class GhostAgent implements Agent {
     }
 
     // choose randomly one of the best moves
-    if( move == -1 ) {
+    if( move == -1 && !this.isMoving() ) {
       move = moves[(int)(Math.random()*count)];
     }
 
     switch(move) {
-      case Baring.N: this.top--;  break;
-      case Baring.E: this.left++; break;
-      case Baring.S: this.top++;  break;
-      case Baring.W: this.left--; break;
-      default: // do nothing
+      case Baring.N: this.goNorth(); break;
+      case Baring.E: this.goEast();  break;
+      case Baring.S: this.goSouth(); break;
+      case Baring.W: this.goWest();  break;
+      default: // do nothing new
     }
 
     // keep track of previous move
     this.previousMove = move;
+    
+    this.processMovement();
+  }
+  
+  private void holdPosition() {
+    this.previousMove = -1;
+    this.turns = 0;
+    this.moves = 0;
+  }
+  
+  private boolean isMoving() {
+    return this.turns != 0 || this.moves > 0;
+  }
+
+  private void goNorth() {
+    switch(this.orientation) {
+      case Baring.W: this.turns = +1; break;
+      case Baring.E: this.turns = -1; break;
+      case Baring.S: this.turns = +2; break;
+      default: // we're already facing north ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void goEast() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = +1; break;
+      case Baring.S: this.turns = -1; break;
+      case Baring.W: this.turns = +2; break;
+      default: // we're already facing east ;-)
+    }
+    this.moves = 1;
+  }
+  
+  private void goSouth() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = -2; break;
+      case Baring.E: this.turns = +1; break;
+      case Baring.W: this.turns = -1; break;
+      default: // we're already facing south ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void goWest() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = -1; break;
+      case Baring.E: this.turns = +2; break;
+      case Baring.S: this.turns = + 1; break;
+      default: // we're already facing west ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void processMovement() {
+    if( this.turns < 0 )      { this.turnLeft();    this.turns++; }
+    else if( this.turns > 0 ) { this.turnRight();   this.turns--; }
+    else if( this.moves > 0 ) { this.moveForward(); this.moves--; }
+  }
+  
+  private void turnRight() {
+    if( this.orientation == Baring.W ) {
+      this.orientation = Baring.N;
+    } else {
+      this.orientation++;
+    }
+  }
+
+  private void turnLeft() {
+    if( this.orientation == Baring.N ) {
+      this.orientation = Baring.W;
+    } else {
+      this.orientation--;
+    }
+  }
+  
+  private void moveForward() {
+    switch(this.orientation) {
+      case Baring.N: this.top--;  break;
+      case Baring.E: this.left++; break;
+      case Baring.S: this.top++;  break;
+      case Baring.W: this.left--; break;
+      default: // impossible ? ;-)
+    }
   }
   
   private int getMax(int[] values) {
--- research/collaborative_diffusion/PacmanAgent.java
+++ research/collaborative_diffusion/PacmanAgent.java
@@ -5,6 +5,10 @@ public class PacmanAgent implements Agent {
   private int orientation;
   private boolean blocked = false;
   
+  // turns are executed, then moves are executed
+  private int turns = 0; // number of turns, positive Right, negative Left
+  private int moves = 0; // number of moves
+    
   // our extra knowledge
   private Maze maze;
   
@@ -72,14 +76,88 @@ public class PacmanAgent implements Agent {
     }
 
     switch(move) {
+      case Baring.N: this.goNorth(); break;
+      case Baring.E: this.goEast();  break;
+      case Baring.S: this.goSouth(); break;
+      case Baring.W: this.goWest();  break;
+      default: // do nothing new
+    }
+    
+    this.processMovement();
+  }
+
+  private void goNorth() {
+    switch(this.orientation) {
+      case Baring.W: this.turns = +1; break;
+      case Baring.E: this.turns = -1; break;
+      case Baring.S: this.turns = +2; break;
+      default: // we're already facing north ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void goEast() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = +1; break;
+      case Baring.S: this.turns = -1; break;
+      case Baring.W: this.turns = +2; break;
+      default: // we're already facing east ;-)
+    }
+    this.moves = 1;
+  }
+  
+  private void goSouth() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = -2; break;
+      case Baring.E: this.turns = +1; break;
+      case Baring.W: this.turns = -1; break;
+      default: // we're already facing south ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void goWest() {
+    switch(this.orientation) {
+      case Baring.N: this.turns = -1; break;
+      case Baring.E: this.turns = +2; break;
+      case Baring.S: this.turns = + 1; break;
+      default: // we're already facing west ;-)
+    }
+    this.moves = 1;
+  }
+
+  private void processMovement() {
+    if( this.turns < 0 )      { this.turnLeft();    this.turns++; }
+    else if( this.turns > 0 ) { this.turnRight();   this.turns--; }
+    else if( this.moves > 0 ) { this.moveForward(); this.moves--; }
+  }
+  
+  private void turnRight() {
+    if( this.orientation == Baring.W ) {
+      this.orientation = Baring.N;
+    } else {
+      this.orientation++;
+    }
+  }
+
+  private void turnLeft() {
+    if( this.orientation == Baring.N ) {
+      this.orientation = Baring.W;
+    } else {
+      this.orientation--;
+    }
+  }
+  
+  private void moveForward() {
+    switch(this.orientation) {
       case Baring.N: this.top--;  break;
       case Baring.E: this.left++; break;
       case Baring.S: this.top++;  break;
       case Baring.W: this.left--; break;
-      default: // do nothing
+      default: // impossible ? ;-)
     }
   }
-  
+
   private int getDistance(int left, int top) {
     int smallestDist = 1000;
     for(Agent agent: this.maze.getAgents()) {
