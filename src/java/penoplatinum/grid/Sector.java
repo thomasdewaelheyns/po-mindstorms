362c74688fdbcab924e5673902fef9260bc8fbe0
Christophe VG <xtof@pumpkin.local>
2012-03-12 12:13:08 +0100

    removed Grid::Tile and implemented it directly on Sector

--- src/java/penoplatinum/grid/Sector.java
+++ src/java/penoplatinum/grid/Sector.java
@@ -14,15 +14,18 @@ import penoplatinum.simulator.mini.Bearing;
 
 public class Sector {
   // back-link to the Grid we live in
-
   private Grid grid;
+
   // links to the adjacent Sectors
   private Sector[] neighbours = new Sector[4];
+
   // position within Grid
   private int left, top;
+
   // walls and the certainty about them
-  private Tile walls = new Tile();
-  private Tile certainty = new Tile();
+  private char walls     = 0;
+  private char certainty = 0;
+
   // the agent currently on this Sector
   private Agent agent;
   // the value associated with this sector
@@ -40,9 +43,9 @@ public class Sector {
 
   public String toString() {
     return " N : " + (this.isKnown(Bearing.N) ? (this.hasWall(Bearing.N) ? "Y" : " ") : "?") + "\n"
-            + " E : " + (this.isKnown(Bearing.E) ? (this.hasWall(Bearing.E) ? "Y" : " ") : "?") + "\n"
-            + " S : " + (this.isKnown(Bearing.S) ? (this.hasWall(Bearing.S) ? "Y" : " ") : "?") + "\n"
-            + " W : " + (this.isKnown(Bearing.W) ? (this.hasWall(Bearing.W) ? "Y" : " ") : "?") + "\n";
+         + " E : " + (this.isKnown(Bearing.E) ? (this.hasWall(Bearing.E) ? "Y" : " ") : "?") + "\n"
+         + " S : " + (this.isKnown(Bearing.S) ? (this.hasWall(Bearing.S) ? "Y" : " ") : "?") + "\n"
+         + " W : " + (this.isKnown(Bearing.W) ? (this.hasWall(Bearing.W) ? "Y" : " ") : "?") + "\n";
   }
 
   // sets the absolute coordinates in the Grid this Sector is placed in
@@ -227,8 +230,7 @@ public class Sector {
   }
 
   protected void inheritWall(int atLocation) {
-    this.walls.withWall(atLocation);
-    this.certainty.withWall(atLocation);
+    this.withWall(atLocation);
     this.grid.wallsNeedRefresh();
   }
 
@@ -244,15 +246,13 @@ public class Sector {
   }
 
   protected void inheritNoWall(int atLocation) {
-    this.walls.withoutWall(atLocation);
-    this.certainty.withWall(atLocation);
+    this.withoutWall(atLocation);
     this.grid.wallsNeedRefresh();
   }
 
   // adds all walls at once
   public Sector addWalls(char walls) {
-    this.walls.withWalls(walls);
-    this.certainty.withWalls((char) 15); // mark all walls as certain
+    this.withWalls(walls);
     // also update neighbours
     this.updateNeighboursWalls();
 
@@ -277,7 +277,7 @@ public class Sector {
   // we use the Boolean class here to be able to return "null" when we don't
   // know anything about the wall.
   public Boolean hasWall(int wall) {
-    return this.certainty.hasWall(wall) ? this.walls.hasWall(wall) : null;
+    return this.knowsWall(wall) ? this.hasRawWall(wall) : null;
   }
 
   public void placeWall(int wall, Boolean value) {
@@ -293,46 +293,120 @@ public class Sector {
   // returns true if there is NO wall and we absolutely can move forward
   // that way
   public boolean givesAccessTo(int location) {
-    return this.certainty.hasWall(location)
-            && !this.walls.hasWall(location);
+    return this.knowsWall(location)
+            && !this.hasRawWall(location);
   }
 
   // returns all wall configuration
   public char getWalls() {
-    return this.walls.getWalls();
+    return this.walls;
   }
 
   // clears the certainty information of the Sector
   public Sector clearCertainty() {
-    this.certainty.clear();
+    this.certainty = 0;
     return this;
   }
 
   // clears all knowledge about a wall
   public Sector clearWall(int atLocation) {
-    this.walls.withoutWall(atLocation);
-    this.certainty.withoutWall(atLocation);
+    this.walls     = this.unsetBit(this.walls, atLocation);
+    this.certainty = this.unsetBit(this.certainty, atLocation);
     return this;
   }
 
   // clears all knowledge about a wall
   public Sector clearWalls() {
-    this.walls.clear();
-    this.certainty.clear();
+    this.walls = 0;
+    this.certainty = 0;
     return this;
   }
 
   // returns the certainty information about the Sector
   public int getCertainty() {
-    return this.certainty.getWalls();
+    return this.certainty;
   }
 
   // determines if the wall at given location is known (to be there or not)
   public boolean isKnown(int atLocation) {
-    return this.certainty.hasWall(atLocation);
+    return this.knowsWall(atLocation);
   }
 
   public boolean isFullyKnown() {
     return this.getCertainty() == 15;
   }
+  
+  private void withWall(int location) { 
+    this.walls     = this.setBit(this.walls,     location);
+    this.certainty = this.setBit(this.certainty, location);
+  }
+
+  public void withWalls(char walls) {
+    this.walls    = walls;
+    this.certainty = 15;
+  }
+
+  public void withoutWall(int location)  { 
+    this.walls     = this.unsetBit(this.walls,     location);
+    this.certainty = this.setBit(this.certainty, location);
+  }
+
+  public void dontKnow(int location)  { 
+    this.certainty = this.unsetBit(this.certainty, location);
+  }
+
+  public boolean hasRawWall(int location) {
+    return this.hasBit(this.walls, location);
+  }
+
+  public boolean knowsWall(int location) {
+    return this.hasBit(this.certainty, location);
+  }
+
+  // public String toString() {
+  //   String bits = "";
+  //   for( int i=0; i<8; i++ ) {
+  //     bits += this.hasBit(this.walls,i) ? "1" : "0";
+  //   }
+  //   bits += "/";
+  //   for( int i=0; i<8; i++ ) {
+  //     bits += this.hasBit(this.certainty,i) ? "1" : "0";
+  //   }
+  //   return bits;
+  // }
+
+  /* elementary bitwise operations */
+
+  // sets one bit at position to 1
+  private char setBit(char data, int p) {
+    data |= (1<<p);
+    return data;
+  }
+
+  // sets one bit at position to 0
+  private char unsetBit(char data, int p) {
+    data &= ~(1<<p);
+    return data;
+  }
+
+  // sets a range of bits, represented by value
+  private char setBits(char data, int start, int length, int value) {
+    data = this.unsetBits( data, start, length );
+    value <<= start;
+    data |= value;
+    return data;
+  }
+
+  // sets a range of bits to 0
+  private char unsetBits(char data, int start, int length) {
+    int mask = ( ( 1 << length ) - 1 ) << start;
+    data &= ~(mask);
+    return data;
+  }
+
+  // checks if at position the bit is set to 1
+  private boolean hasBit(char data, int p) {
+    return ( data & (1<<p) ) != 0;
+  }
+
 }
