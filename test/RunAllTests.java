02065553048a83766961c67a0a997f91901fc1ed
MHGameWorkBase <mhgamework@gmail.com>
2012-04-22 16:03:07 +0200

    Minor compilation fix

--- test/RunAllTests.java
+++ test/RunAllTests.java
@@ -59,7 +59,7 @@ public class RunAllTests {
       penoplatinum.gateway.QueueTest.class,
       penoplatinum.gateway.GatewayClientTest.class,
       penoplatinum.gateway.MessageReceiverTest.class,
-      penoplatinum.simulator.tiles.SectorTest.class
+      penoplatinum.simulator.tiles.SectorTest.class,
       penoplatinum.map.MapHashedTest.class,
       penoplatinum.map.mazeprotocol.BarcodeAtTest.class,
       penoplatinum.map.mazeprotocol.DiscoverTest.class,
