9c5d5cd08d1205b1939966f2d1afc80c18e600ac
Ruben Lapauw <rupsbant@hotmail.com>
2012-04-25 16:15:19 +0200

    oude code verwijderd uit BarcodeAt.java

--- src/java/penoplatinum/map/mazeprotocol/BarcodeAt.java
+++ src/java/penoplatinum/map/mazeprotocol/BarcodeAt.java
@@ -7,33 +7,20 @@ import penoplatinum.map.MapHashed;
 import penoplatinum.simulator.tiles.Sector;
 
 public class BarcodeAt implements Commando {
-  
-  private final boolean INVERT_CODE = true;
-  private final boolean ALLOW_MIRROR = true;
 
   @Override
   public void interpret(MapHashed m, Scanner sc) {
     int x = sc.nextInt();
     int y = sc.nextInt();
     int code = sc.nextInt();
-    int dir = sc.nextInt()-1;
+    int dir = sc.nextInt() - 1;
     Sector s = new Sector();
-    
-    s.addWall((dir+1)%4);
-    s.addWall((dir+3)%4);
-    s.removeWall((dir+2)%4);
+
+    s.addWall((dir + 1) % 4);
+    s.addWall((dir + 3) % 4);
+    s.removeWall((dir + 2) % 4);
     s.removeWall(dir);
-    /*
-     *Dont compress barcodes anymore.
-     *if(INVERT_CODE){
-      code = inverse(code);
-      if(code<0){
-        code = -code-1;
-        dir = (dir+2)%4;
-      }
-    }/**/
     s.addBarcode(code, dir);
-    
     m.put(s, x, -y);
   }
 
@@ -41,21 +28,4 @@ public class BarcodeAt implements Commando {
   public String getName() {
     return "BARCODEAT";
   }
-
-  private int inverse(int code) {
-    for(int i = 0; i<BarcodeBlackBlack.expand.length; i++){
-      if(BarcodeBlackBlack.expand[i] == code){
-        return i;
-      }
-      if(ALLOW_MIRROR && BarcodeBlackBlack.expand[i] == BarcodeTranslator.reverse(code, 6)){
-        return -i-1;
-      }
-    }
-    if(ALLOW_MIRROR){
-      throw new RuntimeException("Incorrect barcode, symmetrical.");
-    } else {
-      throw new RuntimeException("Incorrect barcode, symmetrical or mirrored");
-    }
-  }
-  
 }
--- test/penoplatinum/simulator/entities/SimulatedEntityTest.java
+++ test/penoplatinum/simulator/entities/SimulatedEntityTest.java
@@ -1,109 +1,38 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package penoplatinum.simulator.entities;
 
+import junit.framework.TestCase;
 import penoplatinum.map.Map;
 import penoplatinum.simulator.sensors.Motor;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import penoplatinum.gateway.GatewayClient;
-import penoplatinum.grid.Sector;
-import penoplatinum.map.MapUtil;
 import penoplatinum.robot.Robot;
-import penoplatinum.robot.SimulationRobotAPI;
 import penoplatinum.simulator.Simulator;
-import penoplatinum.simulator.entities.SensorMapping;
-import penoplatinum.simulator.entities.SimulatedEntity;
-import penoplatinum.simulator.view.ViewRobot;
 import penoplatinum.util.Point;
 import static org.mockito.Mockito.*;
 
-/**
- *
- * @author Florian
- */
-public class SimulatedEntityTest {
+public class SimulatedEntityTest extends TestCase {
 
   private SimulatedEntity simEntity;
-
   private Robot mockedRobot;
-
-  private ViewRobot mockedViewRobot;
-
-  private GatewayClient mockedGatewayClient;
-
   private Simulator mockedSimulator;
-
-  private SimulationRobotAPI mockedApi;
-
   private Motor mockedMotorLeft;
-
   private Motor mockedMotorRight;
-
   private Motor mockedMotorSonar;
-
   private Map mockedMap;
-
-  public SimulatedEntityTest() {
-  }
-
-  @BeforeClass
-  public static void setUpClass() throws Exception {
-  }
-
-  @AfterClass
-  public static void tearDownClass() throws Exception {
-  }
 
   @Before
   public void setUp() {
-    this.mockedViewRobot = this.mockViewRobot();
     this.mockedRobot = this.mockRobot();
-    this.mockedApi = this.mockRobotApi();
     this.simEntity = this.createSimulatedEntity();
     this.mockedSimulator = this.mockSimulator();
-    simEntity.useViewRobot(mockedViewRobot);
-    simEntity.setRobotApi(mockedApi);
-    simEntity.useSimulator(mockedSimulator);
     mockedMotorLeft = this.mockMotor();
     mockedMotorRight = this.mockMotor();
     mockedMotorSonar = this.mockMotor();
-    simEntity.setupMotor("L", SensorMapping.M1, SensorMapping.MS1, mockedMotorLeft);
-    simEntity.setupMotor("R", SensorMapping.M2, SensorMapping.MS2, mockedMotorRight);
-    simEntity.setupMotor("S", SensorMapping.M3, SensorMapping.MS3, mockedMotorSonar);
-  }
-
-  @After
-  public void tearDown() {
-  }
-
-  /**
-   * Test of getRobotAPI method, of class SimulatedEntity.
-   */
-  @Test
-  public void testGetRobotAPI() {
-    assertEquals(mockedApi, simEntity.getRobotAPI());
-  }
-
-  /**
-   * Test of setPostition method, of class SimulatedEntity.
-   */
-  @Test
-  public void testSetPostition() {
-    double positionX = 1;
-    double positionY = 18;
-    double direction = 33;
-    simEntity.setPostition(positionX, positionY, direction);
-    assertEquals(positionX, simEntity.getPosX(), 0.05);
-    assertEquals(positionY, simEntity.getPosY(), 0.05);
-    assertEquals((direction + 270) % 360, simEntity.getAngle(), 0.05);
-
+    simEntity.setupMotor(mockedMotorRight, EntityConfig.MOTOR_RIGHT);
+    simEntity.setupMotor(mockedMotorLeft, EntityConfig.MOTOR_LEFT);
+    simEntity.setupMotor(mockedMotorSonar, EntityConfig.MOTOR_SONAR);
+    simEntity.useSimulator(mockedSimulator);
   }
 
   /**
@@ -111,82 +40,10 @@ public class SimulatedEntityTest {
    */
   @Test
   public void testPutRobotAt() {
-    double positionX = 1;
-    double positionY = 18;
-    double direction = 33;
-    assertEquals(simEntity, simEntity.putRobotAt(1, 18, 33));
-    assertEquals(positionX, simEntity.getPosX(), 0.05);
-    assertEquals(positionY, simEntity.getPosY(), 0.05);
-    assertEquals((direction + 270) % 360, simEntity.getAngle(), 0.05);
-  }
-
-  /**
-   * Test of moveRobot method, of class SimulatedEntity.
-   */
-  @Test
-  public void testMoveRobot() {
-
-    simEntity.moveRobot(0.360);
-    verify(this.mockedMotorLeft).rotateBy(740);
-    verify(this.mockedMotorRight).rotateBy(740);
-    simEntity.moveRobot(-5);
-    verify(this.mockedMotorLeft).rotateBy(-10285);
-    verify(this.mockedMotorRight).rotateBy(-10285);
-    simEntity.moveRobot(81);
-    verify(this.mockedMotorLeft).rotateBy(166628);
-    verify(this.mockedMotorRight).rotateBy(166628);
-    simEntity.moveRobot(0.01);
-    verify(this.mockedMotorLeft).rotateBy(20);
-    verify(this.mockedMotorRight).rotateBy(20);
-  }
-
-  /**
-   * Test of turnRobot method, of class SimulatedEntity.
-   */
-  @Test
-  public void testTurnRobot() {
-    simEntity.turnRobot(360);
-    verify(this.mockedMotorLeft).rotateBy(1034);
-    verify(this.mockedMotorRight).rotateBy(-1034);
-    simEntity.turnRobot(-5);
-    verify(this.mockedMotorLeft).rotateBy(-14);
-    verify(this.mockedMotorRight).rotateBy(14);
-    simEntity.turnRobot(81);
-    verify(this.mockedMotorLeft).rotateBy(232);
-    verify(this.mockedMotorRight).rotateBy(-232);
-    simEntity.turnRobot(-90);
-    verify(this.mockedMotorLeft).rotateBy(-258);
-    verify(this.mockedMotorRight).rotateBy(258);
-  }
-
-  /**
-   * Test of stopRobot method, of class SimulatedEntity.
-   */
-  @Test
-  public void testStopRobot() {
-
-    simEntity.moveRobot(0.360);
-    verify(this.mockedMotorLeft).rotateBy(740);
-    verify(this.mockedMotorRight).rotateBy(740);
-    simEntity.stopRobot();
-    verify(this.mockedMotorLeft).stop();
-    verify(this.mockedMotorRight).stop();
-
-  }
-
-  /**
-   * Test of rotateMotorTo method, of class SimulatedEntity.
-   */
-  @Test
-  public void testRotateSensorTo() {
-    simEntity.rotateSonarTo(500);
-    verify(this.mockedMotorSonar).rotateTo(500);
-    simEntity.rotateSonarTo(-50);
-    verify(this.mockedMotorSonar).rotateTo(-50);
-    simEntity.rotateSonarTo(1);
-    verify(this.mockedMotorSonar).rotateTo(1);
-    simEntity.rotateSonarTo(-1);
-    verify(this.mockedMotorSonar).rotateTo(-1);
+    assertEquals(simEntity, simEntity.putRobotAt(1, 18, 303));
+    assertEquals(1, simEntity.getPosX(), 0.05);
+    assertEquals(18, simEntity.getPosY(), 0.05);
+    assertEquals(33, simEntity.getAngle(), 0.05);
   }
 
   /**
@@ -196,23 +53,14 @@ public class SimulatedEntityTest {
   public void testGetters() {
     int x = 5;
     int y = 7;
-    int direction = 30;
+    int direction = 300;
     simEntity.putRobotAt(x, y, direction);
-    assertEquals(simEntity.getPosX(), x, 0.05);
-    assertEquals(simEntity.getPosY(), y, 0.05);
-    assertEquals(simEntity.getAngle(), direction + 270);
-    assertEquals(simEntity.getViewRobot(), this.mockedViewRobot);
-    assertEquals(simEntity.getRobot(), this.mockedRobot);
-  }
-
-  /**
-   * Test of sonarMotorIsMoving method, of class SimulatedEntity.
-   */
-  @Test
-  public void testSonarMotorIsMoving() {
-    simEntity.getSensorValues()[SensorMapping.M3] = 50;
-    assertTrue(simEntity.sonarMotorIsMoving());
-
+    assertEquals(x, simEntity.getPosX(), 0.05);
+    assertEquals(y, simEntity.getPosY(), 0.05);
+    assertEquals(30, simEntity.getAngle());
+    assertEquals(SimulatedViewRobot.class, simEntity.getViewRobot().getClass());
+    assertEquals(direction, simEntity.getViewRobot().getDirection());
+    assertEquals(this.mockedRobot, simEntity.getRobot());
   }
 
   /**
@@ -221,34 +69,22 @@ public class SimulatedEntityTest {
   @Test
   public void testStep() {
     mockedSimulator.useMap(mockedMap);
-    //first test move
-    simEntity.moveRobot(0.01);
-    double positionX1 = simEntity.getPosX();
-    double positionY1 = simEntity.getPosY();
-    when(mockedMotorLeft.getValue()).thenReturn(20);
-    when(mockedMotorRight.getValue()).thenReturn(20);
+    simEntity.putRobotAt(20, 20, 90);
+    //Move and turn at the same time ;)
+    //Make timeslice small enough.
+    when(mockedMotorLeft.getFullAngleTurned()).thenReturn(34.0);
+    when(mockedMotorRight.getFullAngleTurned()).thenReturn(6.0);
     simEntity.step();
+    assertEquals(94.874, simEntity.getDirection(), 0.001);
     verify(mockedMotorLeft).tick(Simulator.TIME_SLICE);
     verify(mockedMotorSonar).tick(Simulator.TIME_SLICE);
     verify(mockedMotorRight).tick(Simulator.TIME_SLICE);
-    verify(mockedMotorLeft, times(2)).getValue();
-    verify(mockedMotorRight, times(2)).getValue();
-    verify(mockedSimulator, times(4)).getMap();
+    verify(mockedMotorLeft, times(2)).getFullAngleTurned();
+    verify(mockedMotorRight, times(2)).getFullAngleTurned();
+    verify(mockedSimulator, times(3)).getMap();
     verify(mockedRobot).step();
-    double positionX2 = simEntity.getPosX();
-    double positionY2 = simEntity.getPosY();
-    assertFalse(positionX1==positionX2);
-    assertFalse(positionY1==positionY2);
-    //test turn
-    simEntity.turnRobot(-5);
-    double direction1 = simEntity.getDirection();
-    when(mockedMotorLeft.getValue()).thenReturn(20-14);
-    when(mockedMotorRight.getValue()).thenReturn(20+14);
-    simEntity.step();
-    double direction2 = simEntity.getDirection();
-    assertFalse(direction1 == direction2);
-
-
+    assertEquals(19.027, simEntity.getPosX(), 0.001);
+    assertEquals(20.0, simEntity.getPosY(), 0.001);
   }
 
   /**
@@ -258,20 +94,15 @@ public class SimulatedEntityTest {
   public void testGetCurrentTileCoordinates() {
     Point point11 = new Point(1, 1);
     Point point22 = new Point(2, 2);
-    Point point = simEntity.getCurrentTileCoordinates();
-    assertEquals(point11, point);
+    assertEquals(point11, simEntity.getCurrentTileCoordinates());
     simEntity.putRobotAt(39, 39, 30);
-    Point point2 = simEntity.getCurrentTileCoordinates();
-    assertEquals(point11, point2);
+    assertEquals(point11, simEntity.getCurrentTileCoordinates());
     simEntity.putRobotAt(40, 40, 0);
-    Point point3 = simEntity.getCurrentTileCoordinates();
-    assertEquals(point22, point3);
+    assertEquals(point22, simEntity.getCurrentTileCoordinates());
     simEntity.putRobotAt(41, 41, 130);
-    Point point4 = simEntity.getCurrentTileCoordinates();
-    assertEquals(point22, point4);
+    assertEquals(point22, simEntity.getCurrentTileCoordinates());
     simEntity.putRobotAt(50, 50, 130);
-    Point point5 = simEntity.getCurrentTileCoordinates();
-    assertEquals(point22, point5);
+    assertEquals(point22, simEntity.getCurrentTileCoordinates());
   }
 
   /**
@@ -283,20 +114,15 @@ public class SimulatedEntityTest {
     Point point22 = new Point(0, 0);
     Point point33 = new Point(1, 1);
     Point point44 = new Point(10, 10);
-    Point point = simEntity.getCurrentOnTileCoordinates();
-    assertEquals(point22, point);
+    assertEquals(point22, simEntity.getCurrentOnTileCoordinates());
     simEntity.putRobotAt(39, 39, 30);
-    Point point2 = simEntity.getCurrentOnTileCoordinates();
-    assertEquals(point11, point2);
+    assertEquals(point11, simEntity.getCurrentOnTileCoordinates());
     simEntity.putRobotAt(40, 40, 0);
-    Point point3 = simEntity.getCurrentOnTileCoordinates();
-    assertEquals(point22, point3);
+    assertEquals(point22, simEntity.getCurrentOnTileCoordinates());
     simEntity.putRobotAt(41, 41, 130);
-    Point point4 = simEntity.getCurrentOnTileCoordinates();
-    assertEquals(point33, point4);
+    assertEquals(point33, simEntity.getCurrentOnTileCoordinates());
     simEntity.putRobotAt(50, 50, 130);
-    Point point5 = simEntity.getCurrentOnTileCoordinates();
-    assertEquals(point44, point5);
+    assertEquals(point44, simEntity.getCurrentOnTileCoordinates());
   }
 
   /**
@@ -322,19 +148,11 @@ public class SimulatedEntityTest {
   }
 
   private SimulatedEntity createSimulatedEntity() {
-    return new SimulatedEntity(mockedRobot);
+    return new SimulatedEntity(mockedRobot, 0);
   }
 
   private Robot mockRobot() {
     return mock(Robot.class);
-  }
-
-  private ViewRobot mockViewRobot() {
-    return mock(ViewRobot.class);
-  }
-
-  private SimulationRobotAPI mockRobotApi() {
-    return mock(SimulationRobotAPI.class);
   }
 
   private Motor mockMotor() {
@@ -342,7 +160,6 @@ public class SimulatedEntityTest {
     when(mockedMotor.setLabel("L")).thenReturn(mockedMotor);
     when(mockedMotor.setLabel("R")).thenReturn(mockedMotor);
     when(mockedMotor.setLabel("S")).thenReturn(mockedMotor);
-    
     return mockedMotor;
   }
 }
