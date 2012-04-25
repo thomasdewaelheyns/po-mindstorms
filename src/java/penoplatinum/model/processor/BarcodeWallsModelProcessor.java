51b21071a20f965ca7a097d0d9621a099adc99d2
Christophe VG <xtof@pumpkin.local>
2012-04-25 23:24:32 +0200

    cleaned up BarcodeWallsModelProcessor + added test

---
+++ src/java/penoplatinum/model/processor/BarcodeWallsModelProcessor.java
@@ -0,0 +1,67 @@
+package penoplatinum.model.processor;
+
+/**
+ * Reponsible for setting the correct walls on a sector when a barcode has
+ * been detected on that sector.
+ * 
+ * A barcode can only be found on a sector with 2 walls. So no sweep is needed
+ * on such a sector.
+ * 
+ * @author Team Platinum
+ */
+
+import penoplatinum.model.Model;
+
+import penoplatinum.grid.LinkedSector;
+
+import penoplatinum.util.Bearing;
+import penoplatinum.model.part.BarcodeModelPart;
+import penoplatinum.model.part.GridModelPart;
+import penoplatinum.model.part.WallsModelPart;
+
+
+public class BarcodeWallsModelProcessor extends ModelProcessor {
+  // decorator boilerplate constructors
+  public BarcodeWallsModelProcessor() { super(); }
+  public BarcodeWallsModelProcessor(ModelProcessor nextProcessor) {
+    super(nextProcessor);
+  }
+
+  private BarcodeModelPart barcode;
+  private GridModelPart    grid;
+  private WallsModelPart   walls;
+  
+  private boolean processedCurrentBarcode = false;
+
+  // override the setModel to setup a reference to the BarcodeModelPart
+  public void setModel(Model model) {
+    super.setModel(model);
+    this.barcode = BarcodeModelPart.from(this.getModel());
+    this.grid    = GridModelPart.from(this.getModel());
+    this.walls   = WallsModelPart.from(this.getModel());
+  }
+
+  public void work() {
+    if( ! this.needToAddWalls() ) { return; }
+
+    Bearing bearing = this.grid.getCurrentBearing();
+    
+    LinkedSector sector = new LinkedSector();
+    sector.setNoWall(bearing);             // no wall in front
+    sector.setNoWall(bearing.reverse());   // no wall at back
+    sector.setWall(bearing.leftFrom());    // wall left
+    sector.setWall(bearing.rightFrom());   // wall right
+
+    this.walls.updateSector(sector);
+
+    this.processedCurrentBarcode = true;
+  }
+  
+  private boolean needToAddWalls() {
+    if( ! this.barcode.isReadingBarcode() ) {
+      this.processedCurrentBarcode = false;
+      return false;
+    }
+    return ! this.processedCurrentBarcode;
+  }
+}
---
+++ test/penoplatinum/model/processor/BarcodeWallsModelProcessorTest.java
@@ -0,0 +1,103 @@
+package penoplatinum.model.processor;
+
+/**
+ * BarcodeWallsModelProcessorTest
+ * 
+ * Tests BarcodeWallsModelProcessor interface is available
+ * 
+ * @author: Team Platinum
+ */
+
+import junit.framework.*; 
+import static org.mockito.Mockito.*;
+
+import penoplatinum.grid.LinkedSector;
+
+import penoplatinum.model.Model;
+
+import penoplatinum.model.part.ModelPartRegistry;
+import penoplatinum.model.part.BarcodeModelPart;
+import penoplatinum.model.part.GridModelPart;
+import penoplatinum.model.part.WallsModelPart;
+
+import penoplatinum.util.Bearing;
+
+
+public class BarcodeWallsModelProcessorTest extends TestCase {
+
+  private BarcodeWallsModelProcessor processor;
+  private Model mockedModel;
+
+  private BarcodeModelPart mockedBarcodeModelPart;
+  private GridModelPart    mockedGridModelPart;
+  private WallsModelPart   mockedWallsModelPart;
+
+
+  public BarcodeWallsModelProcessorTest(String name) { 
+    super(name);
+  }
+
+  public void testSetModel() {
+    this.setup();
+    verify(this.mockedModel).getPart(ModelPartRegistry.BARCODE_MODEL_PART);
+    verify(this.mockedModel).getPart(ModelPartRegistry.GRID_MODEL_PART);
+    verify(this.mockedModel).getPart(ModelPartRegistry.WALLS_MODEL_PART);
+    verifyNoMoreInteractions(this.mockedModel);
+  }
+
+  public void testNoWorkWhenNotReadingBarcode() {
+    this.setup();
+    when(this.mockedBarcodeModelPart.isReadingBarcode()).thenReturn(false);
+    this.processor.work();
+    verifyZeroInteractions(this.mockedGridModelPart);
+  }
+  
+  public void testWork() {
+    this.setup();
+    when(this.mockedBarcodeModelPart.isReadingBarcode()).thenReturn(true);
+    when(this.mockedGridModelPart.getCurrentBearing()).thenReturn(Bearing.N);
+    LinkedSector expected = new LinkedSector();
+    expected.setNoWall(Bearing.N)
+            .setNoWall(Bearing.S)
+            .setWall(Bearing.E)
+            .setWall(Bearing.W);
+    this.processor.work();
+    verify(this.mockedWallsModelPart).updateSector(expected);
+  }
+
+  public void testWorkOnlyOncePerBarcode() {
+    this.setup();
+    when(this.mockedBarcodeModelPart.isReadingBarcode()).thenReturn(true);
+    when(this.mockedGridModelPart.getCurrentBearing()).thenReturn(Bearing.N);
+    this.processor.work();
+    verify(this.mockedGridModelPart).getCurrentBearing(); // once
+    this.processor.work();    
+    verifyNoMoreInteractions(this.mockedGridModelPart);   // not twice
+  }
+
+  private void setup() {
+    this.createProcessor();
+    this.mockModel();
+    this.processor.setModel(this.mockedModel);
+  }
+
+  private void createProcessor() {
+    this.processor = new BarcodeWallsModelProcessor();
+  }
+  
+  private void mockModel() {
+    this.mockedModel = mock(Model.class);
+    // barcode
+    this.mockedBarcodeModelPart = mock(BarcodeModelPart.class);
+    when(this.mockedModel.getPart(ModelPartRegistry.BARCODE_MODEL_PART))
+      .thenReturn(this.mockedBarcodeModelPart);
+    // grid
+    this.mockedGridModelPart = mock(GridModelPart.class);
+    when(this.mockedModel.getPart(ModelPartRegistry.GRID_MODEL_PART))
+      .thenReturn(this.mockedGridModelPart);
+    // walls
+    this.mockedWallsModelPart = mock(WallsModelPart.class);
+    when(this.mockedModel.getPart(ModelPartRegistry.WALLS_MODEL_PART))
+      .thenReturn(this.mockedWallsModelPart);
+  }
+}
