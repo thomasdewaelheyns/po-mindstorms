748f16982b755f366ae49e82c97437fb8b8ab1c2
Thomas De Waelheyns <thomas.dewaelheyns@gmail.com>
2012-04-29 12:42:24 +0200

    Some additional changes.

--- src/java/penoplatinum/pcclients/RobotAdminClient.java
+++ src/java/penoplatinum/pcclients/RobotAdminClient.java
@@ -5,13 +5,12 @@
 package penoplatinum.pcclients;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import penoplatinum.Config;
 import penoplatinum.gateway.MQ;
+import penoplatinum.util.MD5;
 
 /**
  *
@@ -67,16 +66,44 @@ public class RobotAdminClient {
   private static void handleCommand(String input, RobotAdminClient client){
     String[] argumenten = input.split(" ");
     if(argumenten[0].equalsIgnoreCase("FORCESTART")){
-      handleForceStart();
+      client.sendForcedStart();
     }
     else if(argumenten[0].equalsIgnoreCase("load")){
-      handleLoadFile()
+      handleLoadFile(client, argumenten[1]);
+    }
+  }
+  
+  private static void handleLoadFile(RobotAdminClient client, String fileName){
+     BufferedReader in = null;
+
+    try {
+      in = new BufferedReader(new FileReader("logs/"+fileName));
+    }
+    catch(Exception e){
+      System.out.println("could not load the file");
+      return;
+    }
+    try{
+      while (in.ready()) {
+        String line = in.readLine();
+        client.sendLine(line);
+        System.out.println("sent: "+line);
+      }
+    }
+    catch(Exception e){
+      System.out.println("We could locate and open the file, but could not read its content.");
     }
   }
     
-    private void sendForcedStart(){
-      counter++;
-      
-    }
+  private void sendForcedStart(){
+    counter++;
+    String signature = MD5.getHashString(this.secretString + " " + counter + " FORCESTART");
+    rabbit.send(this.name + " PENOPLATINUM_CMD "+ signature +" "+ counter + " FORCESTART\n");
+  }
   
+  private void sendLine(String line){
+    if(line == null)
+      return;
+    this.rabbit.send(line+"\n");
+  }
 }
