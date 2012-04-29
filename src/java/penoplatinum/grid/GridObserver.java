5e718b2190f30bffb481fd6084986436f25d3273
MHGameWorkBase <mhgamework@gmail.com>
2012-04-29 14:56:09 +0200

    Added ObservableGrid.java

---
+++ src/java/penoplatinum/grid/GridObserver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penoplatinum.grid;
+
+/**
+ *
+ * @author MHGameWork
+ */
+public interface GridObserver {
+
+  void agentChanged(Grid g, Agent a);
+}
---
+++ src/java/penoplatinum/grid/ObservableGrid.java
@@ -0,0 +1,129 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penoplatinum.grid;
+
+import penoplatinum.util.Bearing;
+import penoplatinum.util.Point;
+
+/**
+ *
+ * @author MHGameWork
+ */
+public class ObservableGrid implements Grid {
+
+  private final Grid grid;
+  private GridObserver observer;
+
+  ObservableGrid(Grid grid) {
+    this.grid = grid;
+
+    observer = new GridObserver() {
+
+      @Override
+      public void agentChanged(Grid g, Agent a) {
+      }
+    };
+  }
+
+  public ObservableGrid useObserver(GridObserver observer) {
+    this.observer = observer;
+    return this;
+  }
+
+  @Override
+  public Grid add(Sector s, Point position) {
+    grid.add(s, position);
+    return this;
+  }
+
+  @Override
+  public Sector getSectorAt(Point position) {
+    return grid.getSectorAt(position);
+  }
+
+  @Override
+  public Point getPositionOf(Sector sector) {
+    return grid.getPositionOf(sector);
+  }
+
+  @Override
+  public Iterable<Sector> getSectors() {
+    return grid.getSectors();
+  }
+
+  @Override
+  public Grid add(Agent agent, Point position, Bearing bearing) {
+    grid.add(agent, position, bearing);
+    observer.agentChanged(this, agent);
+    return this;
+  }
+
+  @Override
+  public Grid moveTo(Agent agent, Point position, Bearing bearing) {
+    grid.moveTo(agent, position, bearing);
+    observer.agentChanged(this, agent);
+    return this;
+  }
+
+  @Override
+  public Sector getSectorOf(Agent agent) {
+    return grid.getSectorOf(agent);
+  }
+
+  @Override
+  public Bearing getBearingOf(Agent agent) {
+    return grid.getBearingOf(agent);
+  }
+
+  @Override
+  public Agent getAgent(String name) {
+    return grid.getAgent(name);
+  }
+
+  @Override
+  public Agent getAgentAt(Point position) {
+    return grid.getAgentAt(position);
+  }
+
+  @Override
+  public Iterable<Agent> getAgents() {
+    return grid.getAgents();
+  }
+
+  @Override
+  public int getMinLeft() {
+    return grid.getMinLeft();
+  }
+
+  @Override
+  public int getMaxLeft() {
+    return grid.getMaxLeft();
+  }
+
+  @Override
+  public int getMinTop() {
+    return grid.getMinTop();
+  }
+
+  @Override
+  public int getMaxTop() {
+    return grid.getMaxTop();
+  }
+
+  @Override
+  public int getWidth() {
+    return grid.getWidth();
+  }
+
+  @Override
+  public int getHeight() {
+    return grid.getHeight();
+  }
+
+  @Override
+  public int getSize() {
+    return grid.getSize();
+  }
+}
