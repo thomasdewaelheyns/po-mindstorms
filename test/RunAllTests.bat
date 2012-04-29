e930bb2fd000a302af7747a1537ddf279177c27d
MHGameWorkBase <mhgamework@gmail.com>
2012-04-29 11:13:25 +0200

    Minor fix to RunAllTests.bat

--- test/RunAllTests.bat
+++ test/RunAllTests.bat
@@ -1,3 +1,4 @@
+mkdir build
 javac -cp .;..\src\java;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar -d build -Xlint:unchecked -Xlint:deprecation RunAllTests.java
 
 java -cp build;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar junit.textui.TestRunner RunAllTests
