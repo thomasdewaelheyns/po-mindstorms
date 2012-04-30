e21c52297fa2179041135cff142217791e35c2f3
MHGameWork-Laptop <mhgamework@gmail.com>
2012-04-30 20:15:13 +0200

    Added new bat file

--- test/RunAllTests.bat
+++ test/RunAllTests.bat
@@ -1,5 +1,4 @@
-mkdir build
-javac -cp .;..\src\java;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar -d build -Xlint:unchecked -Xlint:deprecation RunAllTests.java
+javac -cp .;..\src\java;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar;..\lib\rabbitmq-client.jar;..\lib\commons-io-1.2.jar;..\lib\commons-collections-3.2.1.jar;..\lib\commons-cli-1.2.jar -d build -Xlint:unchecked -Xlint:deprecation RunAllTests.java
 
-java -cp build;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar junit.textui.TestRunner RunAllTests
+java -cp build;..\lib\junit-4.10.jar;..\lib\mockito-all-1.9.0.jar;..\lib\rabbitmq-client.jar;..\lib\commons-io-1.2.jar;..\lib\commons-collections-3.2.1.jar;..\lib\commons-cli-1.2.jar junit.textui.TestRunner RunAllTests
 pause
