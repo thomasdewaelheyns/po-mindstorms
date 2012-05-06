16c25c22f5207774b734b48fca62ce6fdd2b42a1
Thomas De Waelheyns <thomas.dewaelheyns@gmail.com>
2012-05-06 15:47:35 +0200

    Fixed DashboardReporter.java and DashboardReporterTest.java. Both should work.

--- src/java/penoplatinum/reporter/DashboardReporter.java
+++ src/java/penoplatinum/reporter/DashboardReporter.java
@@ -8,20 +8,21 @@ package penoplatinum.reporter;
  * @author Team Platinum
  */
 
-import java.util.ArrayList;
-
 import penoplatinum.Config;
-import penoplatinum.model.GhostModel;
-
+import penoplatinum.gateway.GatewayClient;
+import penoplatinum.grid.Agent;
 import penoplatinum.grid.Grid;
 import penoplatinum.grid.Sector;
-import penoplatinum.grid.agent.Agent;
-
-import penoplatinum.gateway.GatewayClient;
+import penoplatinum.model.GhostModel;
 import penoplatinum.model.Model;
+import penoplatinum.model.part.BarcodeModelPart;
+import penoplatinum.model.part.GridModelPart;
+import penoplatinum.model.part.LightModelPart;
+import penoplatinum.model.part.SensorModelPart;
 import penoplatinum.robot.GhostRobot;
 import penoplatinum.robot.Robot;
 import penoplatinum.util.Bearing;
+import penoplatinum.util.Point;
 
 public class DashboardReporter implements Reporter {
 
@@ -32,6 +33,7 @@ public class DashboardReporter implements Reporter {
   StringBuilder sb = new StringBuilder();
   long nextTime = 0;
 
+  @Override
   public DashboardReporter useGatewayClient(GatewayClient client) {
     this.client = client;
     return this;
@@ -43,49 +45,35 @@ public class DashboardReporter implements Reporter {
     return this;
   }
 
-  public DashboardReporter report() {
-    if( this.client == null || this.robot == null || this.model == null ) {
-      System.err.println( "DashboardReporter: missing client/robot/model" );
-      return this;
-    }
-    
-    this.sendModelDeltas();
-
-    this.sendGrid("myGrid", this.model.getGridPart().getGrid());
-
-    ArrayList<Sector> changed = this.model.getGridPart().getChangedSectors();
-    for (int i = 0; i < changed.size(); i++) {
-      this.sendSectorWalls(this.robot.getName(), "myGrid", changed.get(i));
-    }
-    return this;
-  }
-
-  private void sendModelDeltas() {
+  private void sendModelDeltas(Agent agent) {
     long now = System.nanoTime();
     if(now < this.nextTime) { return; }
     this.nextTime = now + 300000000;
-
-    Sector currentSector = this.model.getGridPart().getAgent().getSector();
+    Point pos = this.getMainGrid(this.model).getPositionOf(agent);
+    Sector currentSector = this.getMainGrid(this.model).getSectorAt(pos);
     
+    SensorModelPart sensorPart = SensorModelPart.from(this.model);
+    BarcodeModelPart barcodePart = BarcodeModelPart.from(this.model);
+    LightModelPart lightPart = LightModelPart.from(this.model);
     this.clear()
         .add(this.robot.getName())                                       .c()
-        .add(this.model.getSensorPart().getLightSensorValue())           .c()
-        .add(this.model.getLightPart().getCurrentLightColor().toString()).c()
-        .add((int)this.model.getLightPart().getAverageLightValue())      .c()
-        .add(this.model.getBarcodePart().getBarcode())                   .c()
-        .add(this.model.getSensorPart().getSonarAngle()                  .c()
-        .add(this.model.getSensorPart().getSonarDistance())              .c()
-        .add(this.model.getSensorPart().getSonarValue(0))                .c()
-        .add(this.model.getSensorPart().getSonarValue(1))                .c()
-        .add(this.model.getSensorPart().getSonarValue(2))                .c()
-        .add(this.model.getSensorPart().getSonarValue(3))                .c()
-        .add(this.model.getSensorPart().getSonarValue(4))                .c()
+        .add(sensorPart.getLightSensorValue())           .c()
+        .add(lightPart.getCurrentLightColor().toString()).c()
+        .add((int)lightPart.getAverageLightValue())      .c()
+        .add(barcodePart.getLastBarcodeValue())                   .c()
+        .add(sensorPart.getSonarAngle())                  .c()
+        .add(sensorPart.getSonarDistance())              .c()
+        .add(sensorPart.getIRValue(0))                .c()
+        .add(sensorPart.getIRValue(1))                .c()
+        .add(sensorPart.getIRValue(2))                .c()
+        .add(sensorPart.getIRValue(3))                .c()
+        .add(sensorPart.getIRValue(4))                .c()
         // ir_dist the shortest distance        
         .add(-1)                                                         .c()
-        .add((int)currentSector.getWalls()).c();
+        .add(this.getWalls(currentSector)).c();
 
-    for(int atLocation=Bearing.N; atLocation<=Bearing.W; atLocation++ ) {
-      Sector neighbour = currentSector.getNeighbour(atLocation);
+    for(Bearing direction: Bearing.NESW){
+      Sector neighbour = currentSector.getNeighbour(direction);
       this.add(neighbour == null ? 0 : neighbour.getValue())             .c();
     }
 
@@ -121,7 +109,7 @@ public class DashboardReporter implements Reporter {
   private void sendSectorWalls(String name, String grid, Sector sector) {
     this.clear()
         .addSector(name, grid, sector)                                   .c()
-        .add((int)sector.getWalls())
+        .add(this.getWalls(sector))
         .sendBuffer(Config.BT_WALLS);
   }
 
@@ -133,10 +121,11 @@ public class DashboardReporter implements Reporter {
   }
 
   private DashboardReporter addSector(String name, String grid, Sector sector) {
+    Grid mainGrid = this.getMainGrid(this.model);
     this.add(name)                                                       .c()
         .add(grid)                                                       .c()
-        .add(sector.getLeft())                                           .c()
-        .add(sector.getTop());
+        .add(mainGrid.getPositionOf(sector).getX())                      .c()
+        .add(mainGrid.getPositionOf(sector).getY());
     return this;
   }
 
@@ -146,13 +135,16 @@ public class DashboardReporter implements Reporter {
   }
 
   private DashboardReporter sendSectorAgent(String name, String grid, Agent agent) {
+    Grid mainGrid = this.getMainGrid(this.model);
+    Point pos = mainGrid.getPositionOf(agent);
+    Sector sec = mainGrid.getSectorAt(pos);
     this.clear()
         .add(name)                                                       .c()
         .add(grid)                                                       .c()
         .add(agent.getName())                                            .c()
-        .add(agent.getSector().getLeft())                                .c()
-        .add(agent.getSector().getTop())                                 .c()
-        .add(agent.getBearing() + 1)                         .c()
+        .add(pos.getX())                         .c()
+        .add(pos.getY())                         .c()
+        .add(translateBearingToInt(mainGrid.getBearingOf(agent)))          .c()
         .add("white")
         .sendBuffer(Config.BT_AGENTS);
     return this;
@@ -181,5 +173,48 @@ public class DashboardReporter implements Reporter {
   private void sendBuffer(int channel) {
     this.client.send(this.sb.toString(), channel); 
   }
+  
+  private Grid getMainGrid(Model model){
+    GridModelPart gridPart = GridModelPart.from(model);
+    return gridPart.getMyGrid();
+  }
+  
+  private int getWalls(Sector sector){
+    int code = 0;
+    code += ((!sector.knowsWall(Bearing.N) || sector.hasNoWall(Bearing.N)) ? 0 : 1)<<3;
+    code += ((!sector.knowsWall(Bearing.E) || sector.hasNoWall(Bearing.E)) ? 0 : 1)<<2;
+    code += ((!sector.knowsWall(Bearing.S) || sector.hasNoWall(Bearing.S)) ? 0 : 1)<<1;
+    code += ((!sector.knowsWall(Bearing.W) || sector.hasNoWall(Bearing.W)) ? 0 : 1);
+    return code;
+  }
 
+  @Override
+  public Reporter reportModelUpdate(Agent agent) {
+    this.sendModelDeltas(agent);
+    return this;
+  }
+
+  @Override
+  public Reporter reportSectorUpdate(Sector sector) {
+    this.sendSectorWalls(this.robot.getName(), "myGrid", sector);
+    return this;
+  }
+
+  @Override
+  public Reporter reportAgentUpdate(Agent agent) {
+    this.reportAgent(agent);
+    return this;
+  }
+  
+  private int translateBearingToInt(Bearing bear){
+    if(bear == Bearing.N)
+      return 1;
+    if(bear == Bearing.E)
+      return 2;
+    if(bear == Bearing.S)
+      return 3;
+    if(bear == Bearing.W)
+      return 4;
+    return 0;
+  }
 }
--- src/java/penoplatinum/reporter/Reporter.java
+++ src/java/penoplatinum/reporter/Reporter.java
@@ -10,8 +10,7 @@ package penoplatinum.reporter;
  */
 
 import penoplatinum.gateway.GatewayClient;
-
-import penoplatinum.grid.agent.Agent;
+import penoplatinum.grid.Agent;
 import penoplatinum.grid.Sector;
 
 
@@ -19,12 +18,7 @@ public interface Reporter {
   public Reporter useGatewayClient(GatewayClient client);
 
   // the reporter can process updates to the information
-  public Reporter reportModelUpdate();
+  public Reporter reportModelUpdate(Agent agent);
   public Reporter reportSectorUpdate(Sector sector);
   public Reporter reportAgentUpdate(Agent agent);
-
-  // the reporter can process events
-  public Reporter reportEnterSector(Agent me, Sector sector);
-  public Reporter reportFoundSector(Agent me, Sector sector);
-  public Reporter reportFoundAgent(Agent me, Sector sector, Agent agent);
 }
